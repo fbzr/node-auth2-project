ca261b0df46d6b6f139d88176fd872fd7ce77dce
fbzr <fabricio.spb@gmail.com>
2020-04-22 13:54:05 -0700

    fix migration - add unique to username

--- data/migrations/20200421090541_create-users-table.js
+++ data/migrations/20200421090541_create-users-table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', tbl => {
         tbl.increments();
-        tbl.string('username').notNullable();
+        tbl.string('username').notNullable().unique();
         tbl.string('password').notNullable();
         tbl.string('department').notNullable();
     });
